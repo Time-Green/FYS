2ae506b940008048b7d8a9338e130c2adb3d16ba
JoppieToppie <topjordy@gmail.com>
2019-11-22 13:30:10 +0100

    StructureDesigner prototype v2

--- StructureDesigner/StructureDesigner/MainWindow.xaml.cs
+++ StructureDesigner/StructureDesigner/MainWindow.xaml.cs
@@ -8,14 +8,26 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using MahApps.Metro.Controls.Dialogs;
+using Microsoft.Win32;
+using Newtonsoft.Json;
 using Path = System.IO.Path;
 
 namespace StructureDesigner
 {
-
     public partial class MainWindow
     {
         private const int TileSize = 50;
+        private const int LayerAmount = 4;
+        private const int EditorGridWidth = 100;
+        private const int EditorGridHeight = 100;
+
+        private readonly List<Image[,]> _layers = new List<Image[,]>();
+        private int _selectedLayer;
+        private string _fysDataDirectory;
+        private bool isMouseDownOnCanvas;
+
+        private Image selectedImage;
 
         public MainWindow()
         {
@@ -36,11 +48,21 @@ namespace StructureDesigner
                 Header = "Layer"
             });
 
-            // Populate list
-            LayerView.Items.Add("Background");
-            LayerView.Items.Add("Layer 1");
-            LayerView.Items.Add("Layer 2");
-            LayerView.Items.Add("Layer 3");
+            for (var i = 0; i < LayerAmount; i++)
+            {
+                if (i == 0)
+                {
+                    LayerView.Items.Add("Background");
+                }
+                else
+                {
+                    LayerView.Items.Add("Layer " + i);
+                }
+
+                var layerArray = new Image[EditorGridWidth, EditorGridHeight];
+
+                _layers.Add(layerArray);
+            }
 
             LayerView.SelectedItem = "Background";
         }
@@ -48,7 +70,7 @@ namespace StructureDesigner
         private void AddGridLines()
         {
             //horizontal lines
-            for (var i = 0; i < 100; i++)
+            for (var i = 0; i < EditorGridWidth; i++)
             {
                 var line = new Line
                 {
@@ -64,7 +86,7 @@ namespace StructureDesigner
             }
 
             //vertical lines
-            for (var i = 0; i < 100; i++)
+            for (var i = 0; i < EditorGridHeight; i++)
             {
                 var line = new Line
                 {
@@ -84,7 +106,7 @@ namespace StructureDesigner
         {
             var currentDirectory = Directory.GetCurrentDirectory();
             var fysDirectory = currentDirectory.Split(new[] {"FYS"}, StringSplitOptions.None)[0];
-            var fysDataDirectory = Path.Combine(fysDirectory, "FYS", "data");
+            _fysDataDirectory = Path.Combine(fysDirectory, "FYS", "data");
 
             var allFiles = new List<string>();
 
@@ -100,7 +122,7 @@ namespace StructureDesigner
 
             foreach (var category in categoriesToUse)
             {
-                var directory = Path.Combine(fysDataDirectory, "Sprites", category);
+                var directory = Path.Combine(_fysDataDirectory, "Sprites", category);
                 var allTiles = Directory.GetFiles(directory, "*.*", SearchOption.AllDirectories).Where(s => s.EndsWith(".png") || s.EndsWith(".jpg")).ToList();
 
                 allFiles.AddRange(allTiles);
@@ -123,52 +145,231 @@ namespace StructureDesigner
             }
         }
 
-        private static void ImageOnPreviewMouseDown(object sender, MouseButtonEventArgs e)
+        private void ImageOnPreviewMouseDown(object sender, MouseButtonEventArgs e)
         {
-            DragDrop.DoDragDrop((DependencyObject)sender, ((Image)sender).Source, DragDropEffects.Copy);
+            if (selectedImage != null)
+            {
+                selectedImage.Margin = new Thickness(5);
+                selectedImage.Width = 50;
+                selectedImage.Height = 50;
+            }
+
+            selectedImage = (Image)sender;
+            selectedImage.Margin = new Thickness(1);
+            selectedImage.Width = 58;
+            selectedImage.Height = 58;
         }
 
-        private void Canvas_Drop(object sender, DragEventArgs e)
+        private void Canvas_OnMouseDown(object sender, MouseButtonEventArgs e)
         {
-            foreach (var format in e.Data.GetFormats())
+            isMouseDownOnCanvas = true;
+
+            var gridPoint = GetGridPos(e.GetPosition(Canvas));
+
+            AddTile(gridPoint, selectedImage.Source, _selectedLayer);
+        }
+
+        private void Canvas_OnMouseUp(object sender, MouseButtonEventArgs e)
+        {
+            isMouseDownOnCanvas = false;
+        }
+
+        private void Canvas_OnMouseMove(object sender, MouseEventArgs e)
+        {
+            if (!isMouseDownOnCanvas)
             {
-                if (!(e.Data.GetData(format) is ImageSource imageSource))
+                return;
+            }
+
+            var gridPoint = GetGridPos(e.GetPosition(Canvas));
+
+            AddTile(gridPoint, selectedImage.Source, _selectedLayer);
+        }
+
+        private void AddTile(Point gridPoint, ImageSource imageSource, int layer)
+        {
+            var existingImageOnGridPoint = _layers[layer][(int) gridPoint.X / TileSize, (int) gridPoint.Y / TileSize];
+
+            if (existingImageOnGridPoint != null)
+            {
+                //same image
+                if (existingImageOnGridPoint.Source == imageSource)
                 {
-                    continue;
+                    return;
                 }
 
-                var img = new Image
-                {
-                    Height = TileSize,
-                    Width = TileSize,
-                    Source = imageSource
-                };
-
-                ((Canvas)sender).Children.Add(img);
-
-                AlignToGrid(img, e.GetPosition(Canvas));
+                Canvas.Children.Remove(existingImageOnGridPoint);
             }
-        }
 
-        private static void AlignToGrid(UIElement img, Point dropPoint)
-        {
-            var targetX = GetGridPos(dropPoint.X);
-            var targetY = GetGridPos(dropPoint.Y);
-
-            Canvas.SetLeft(img, targetX);
-            Canvas.SetTop(img, targetY);
-        }
-
-        private static int GetGridPos(double dropPos)
-        {
-            var flooredValue = (int) Math.Floor(dropPos);
-
-            while (flooredValue % TileSize != 0)
+            var img = new Image
             {
-                flooredValue--;
+                Height = TileSize,
+                Width = TileSize,
+                Source = imageSource
+            };
+
+            Canvas.Children.Add(img);
+
+            AddToGrid(img, gridPoint);
+        }
+
+        private void AddToGrid(Image img, Point gridPoint)
+        {
+            Canvas.SetLeft(img, gridPoint.X);
+            Canvas.SetTop(img, gridPoint.Y);
+
+            _layers[_selectedLayer][(int)gridPoint.X / TileSize, (int)gridPoint.Y / TileSize] = img;
+        }
+
+        private static Point GetGridPos(Point dropPoint)
+        {
+            var flooredXValue = (int) Math.Floor(dropPoint.X);
+
+            while (flooredXValue % TileSize != 0)
+            {
+                flooredXValue--;
+            }
+            
+            var flooredYValue = (int)Math.Floor(dropPoint.Y);
+
+            while (flooredYValue % TileSize != 0)
+            {
+                flooredYValue--;
             }
 
-            return flooredValue;
+            return new Point(flooredXValue, flooredYValue);
+        }
+
+        private void LayerView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            _selectedLayer = LayerView.SelectedIndex;
+        }
+
+        private async void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            var result = await this.ShowInputAsync("Enter name", "Enter a name to save the structure as");
+
+            if (!string.IsNullOrWhiteSpace(result))
+            {
+                Save(result);
+            }
+        }
+        private void LoadButton_Click(object sender, RoutedEventArgs e)
+        {
+            var fileDialog = new OpenFileDialog
+            {
+                InitialDirectory = Path.Combine(_fysDataDirectory, "Structures"),
+                Filter = "json files (*.json)|*.json"
+            };
+
+            var selectedFile = fileDialog.ShowDialog();
+
+            if (!selectedFile.HasValue || string.IsNullOrEmpty(fileDialog.FileName))
+            {
+                return;
+            }
+
+            var data = File.ReadAllText(fileDialog.FileName);
+
+            var structure = JsonConvert.DeserializeObject<List<List<string>>>(data);
+
+            var layerIndex = 0;
+
+            foreach (var layer in structure)
+            {
+                foreach (var tile in layer)
+                {
+                    var split = tile.Split('|');
+
+                    if (split.Length == 1)
+                    {
+                        continue;
+                    }
+
+                    var x = int.Parse(split[0]);
+                    var y = int.Parse(split[1]);
+                    var file = split[2];
+                    var point = new Point(x * TileSize, y * TileSize);
+                    var fullPath = _fysDataDirectory + file;
+
+                    AddTile(point, new BitmapImage(new Uri(fullPath)), layerIndex);
+                }
+
+                layerIndex++;
+            }
+        }
+
+        private void Save(string saveName)
+        {
+            var minXPos = EditorGridWidth;
+            var minYPos = EditorGridHeight;
+            var maxXPos = -1;
+            var maxYPos = -1;
+
+            for (var y = 0; y < EditorGridHeight; y++)
+            {
+                for (var x = 0; x < EditorGridWidth; x++)
+                {
+                    var existingImageOnGrid = _layers[0][x, y];
+
+                    if (existingImageOnGrid != null)
+                    {
+                        if (x < minXPos)
+                        {
+                            minXPos = x;
+                        }
+                            
+                        if (y < minYPos)
+                        {
+                            minYPos = y;
+                        }
+
+                        if (x > maxXPos)
+                        {
+                            maxXPos = x;
+                        }
+
+                        if (y > maxYPos)
+                        {
+                            maxYPos = y;
+                        }
+                    }
+                }
+            }
+
+            var saveList = new List<List<string>>();
+
+            for (var layer = 0; layer < LayerAmount; layer++)
+            {
+                var currentLayer = new List<string>();
+                
+                for (var currentY = minYPos; currentY <= maxYPos; currentY++)
+                {
+                    for (var currentX = minXPos; currentX <= maxXPos; currentX++)
+                    {
+                        var img = _layers[layer][currentX, currentY];
+
+                        if (img == null)
+                        {
+                            currentLayer.Add("");
+                            continue;
+                        }
+
+                        var fullSource = img.Source.ToString();
+                        var removedFileTitle = fullSource.Remove(0, 8);
+                        var replacedFullSource = removedFileTitle.Replace("/", "\\");
+                        var baseRemovedPath = replacedFullSource.Replace(_fysDataDirectory, string.Empty);
+
+                        currentLayer.Add(currentX + "|" + currentY + "|" + baseRemovedPath);
+                    }
+                }
+
+                saveList.Add(currentLayer);
+            }
+
+            var saveString = JsonConvert.SerializeObject(saveList, Formatting.Indented);
+
+            File.WriteAllText(Path.Combine(_fysDataDirectory, "Structures", saveName + ".json"), saveString);
         }
     }
 }
